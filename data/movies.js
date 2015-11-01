e1c37d49c4ea3493715d5cbc3dd9d702fdacceeb
rameshtakeoff <ramesh.yadav@takeoff.net.in>
2015-11-01 11:35:00 +0530

    Food

---
+++ data/movies.js
@@ -0,0 +1,210 @@
+var movies = [
+  {
+    "name": "Balushahi",
+    "outline": "",
+    "rating": 9.3,
+    "Productimg": "imgProd/Angel_food_cake.jpg",
+    "year": 1994,
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": "Rasgulla",
+    "outline": "",
+    "rating": 8.3,
+    "Productimg": "imgProd/Rasagulla.jpg",
+    "year": 1994,
+
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  
+    {
+    "name": "Jalebi",
+    "outline": "",
+    "rating": 8.8,
+    "Productimg": "imgProd/Jalebi.jpg",
+    "year": 1994,
+  
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+     {
+    "name": "Balushahi",
+    "outline": "",
+    "rating": 9.3,
+    "Productimg": "imgProd/Angel_food_cake.jpg",
+    "year": 1994,
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": "Rasgulla",
+    "outline": "",
+    "rating": 8.3,
+    "Productimg": "imgProd/Rasagulla.jpg",
+    "year": 1994,
+
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  
+    {
+    "name": "Jalebi",
+    "outline": "",
+    "rating": 8.8,
+    "Productimg": "imgProd/Jalebi.jpg",
+    "year": 1994,
+  
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+   {
+    "name": "Balushahi",
+    "outline": "",
+    "rating": 9.3,
+    "Productimg": "imgProd/Angel_food_cake.jpg",
+    "year": 1994,
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": "Rasgulla",
+    "outline": "",
+    "rating": 8.3,
+    "Productimg": "imgProd/Rasagulla.jpg",
+    "year": 1994,
+
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  
+    {
+    "name": "Jalebi",
+    "outline": "",
+    "rating": 8.8,
+    "Productimg": "imgProd/Jalebi.jpg",
+    "year": 1994,
+  
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+    {
+    "name": "Balushahi",
+    "outline": "",
+    "rating": 9.3,
+    "Productimg": "imgProd/Angel_food_cake.jpg",
+    "year": 1994,
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": "Rasgulla",
+    "outline": "",
+    "rating": 8.3,
+    "Productimg": "imgProd/Rasagulla.jpg",
+    "year": 1994,
+
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  
+    {
+    "name": "Jalebi",
+    "outline": "",
+    "rating": 8.8,
+    "Productimg": "imgProd/Jalebi.jpg",
+    "year": 1994,
+  
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  }
+
+
+
+
+]
